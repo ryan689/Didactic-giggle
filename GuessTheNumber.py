9bd7608815818abeea6f6574039750f59f1b4660
Ryan Albertson <rjalbert@email.uark.edu>
2018-05-14 20:55:00 -0500

    Add comment description

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,1 +1,8 @@
-# Guess the number
+'''
+The program will first randomly generate a number unknown to the user. The user needs to guess what that 
+number is. (In other words, the user needs to be able to input information.) If the user's guess is wrong, 
+the program should return some sort of indication as to how wrong (e.g. The number is too high or too low.) 
+If the user guesses correctly, a positive indication should appear. 
+You'll need functions to check if the user input is an actual number, to see the difference between the 
+inputted number and the randomly generated numbers, and to then compare the numbers.
+'''
