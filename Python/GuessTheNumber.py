f5b8780dfff21eff70fb706aaaeaaddc67a55eaf
Ryan Albertson <rjalbert@email.uark.edu>
2018-05-14 21:08:13 -0500

    Add initial comments

--- Python/GuessTheNumber.py
+++ Python/GuessTheNumber.py
@@ -1,1 +1,17 @@
+'''
+The program will first randomly generate a number unknown to the user. The user needs to guess what that 
+number is. (In other words, the user needs to be able to input information.) If the user's guess is wrong, 
+the program should return some sort of indication as to how wrong (e.g. The number is too high or too low.) 
+If the user guesses correctly, a positive indication should appear. 
+You'll need functions to check if the user input is an actual number, to see the difference between the 
+inputted number and the randomly generated numbers, and to then compare the numbers.
+'''
 
+
+# 1) Randomly generate a number unknown to the user
+
+# 2) User should guess what that number is
+
+# 3) If guess is wrong, let them know too high / too low
+
+# 4) If guess is correct, let them know
